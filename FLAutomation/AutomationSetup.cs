afea7ba7e563d56a69e509645f5fdc5ca4afc89e
Kuntal Mehta <KuntalM@sfwltd.co.uk>
2018-04-16 19:42:12 +0530

    Add project files.

---
+++ FLAutomation/AutomationSetup.cs
@@ -0,0 +1,72 @@
+﻿using log4net;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+using System.Threading;
+
+namespace FLAutomation
+{
+    public class AutomationBase
+    {
+        #region Global Variables
+        public static string baseURL = ConfigurationManager.AppSettings["baseURL"].ToString();
+        public static string UserName = ConfigurationManager.AppSettings["UserName"].ToString();
+        public static string Password = ConfigurationManager.AppSettings["Password"].ToString();
+        public static IWebDriver driver = null;
+        private string Browser = ConfigurationManager.AppSettings["Browser"].ToString();
+        private string DriverPath = AppDomain.CurrentDomain.BaseDirectory + "\\Driver\\";
+        public static IJavaScriptExecutor js;
+
+        //public static ExtentReportBase Report = new ExtentReportBase();
+        #endregion
+
+        #region CommonMethods
+        [OneTimeSetUp]
+        public void TestInitialize()
+        {
+            try
+            {
+                driver = new ChromeDriver(DriverPath);
+                js = driver as IJavaScriptExecutor;
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl(baseURL);
+                signin();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        [OneTimeTearDown]
+        public void TestCleanup()
+        {
+            try
+            {
+
+                driver.Close();
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        #endregion
+
+
+        public void signin()
+        {
+            driver.FindElement(By.Id("userid")).Clear();
+            driver.FindElement(By.Id("userid")).SendKeys("CANREQ");
+            driver.FindElement(By.Name("pwd1")).Clear();
+            driver.FindElement(By.Name("pwd1")).SendKeys("Password10");
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("butSignOn")).Click();
+        }
+    }
+}
---
+++ FLAutomation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace FLAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutomationBase ab = new AutomationBase();
+            ab.TestInitialize();
+            Thread.Sleep(100);
+            ab.TestCleanup();
+        }
+    }
+}
